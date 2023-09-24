833d04e16a9c517a032b87b02581a1fc926aeb63
baez91 <baez_ivan-@hotmail.com>
2023-09-24 19:36:52 -0300

    requeri el archivo app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+const sumar = require("./modulos/suma")
+const restar = require("./modulos/resta")
+const dividir = require("./modulos/division")
+const multiplicar = require("./modulos/multiplicacion")
+
+const {leerArchivo, guardarArchivo} = require("./modulos/leeryescribir")
+
+let variableArchivo = leerArchivo()
--- modulos/leer.js
+++ modulos/leer.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 
 function leerArchivo(){
-    let path = fs.readFileSync("../data/registro.json","utf-8")
+    let path = fs.readFileSync("./data/registro.json","utf-8")
     let array = JSON.parse(path)
     return array
 }
