435e56a6a323b96748cd3326f76f4f92cb0e9ea9
baez91 <baez_ivan-@hotmail.com>
2023-09-24 19:46:51 -0300

    agregue archivo leer

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const sumar = require("./modulos/suma")
 const restar = require("./modulos/resta")
 const dividir = require("./modulos/division")
 const multiplicar = require("./modulos/multiplicacion")
+const {escribir}=require("./modulos/escribir")
+const {leer}=require("./modulos/leer")
 
-const {leerArchivo, guardarArchivo} = require("./modulos/leeryescribir")
-
-let variableArchivo = leerArchivo()
+let variableArchivo = leer()
